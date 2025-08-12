4037320edf80012c6e0bcb5718765ded26318f93
suho <yousuho1232@naver.com>
2025-08-12 10:18:36 +0900

    feat: Render.yaml 파일 추가 - MariaDB, Redis, 백엔드 및 프론트엔드 서비스 정의

--- backend/src/main/java/com/suho149/liveauction/global/config/SecurityConfig.java
+++ backend/src/main/java/com/suho149/liveauction/global/config/SecurityConfig.java
@@ -131,7 +131,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 프론트엔드 출처 허용
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://*.onrender.com")); // 프론트엔드 출처 허용
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
