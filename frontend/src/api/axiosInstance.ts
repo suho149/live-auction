0be7c6d8d134782a93428fab6ce6f2b67b2d4fdd
suho <yousuho1232@naver.com>
2025-08-14 14:18:43 +0900

    fix: API 기본 URL을 운영 환경에 맞게 수정

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import useAuthStore from '../hooks/useAuthStore'; // zustand 스토어 import
 
-//export const API_BASE_URL = 'http://localhost:8080';
-//export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '/';
-export const API_BASE_URL = '/';
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '/';
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
