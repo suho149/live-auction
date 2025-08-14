5b23310289e1b379bc8d6bb70facbb5dec6a24da
suho <yousuho1232@naver.com>
2025-08-14 14:10:20 +0900

    fix: API 기본 URL을 운영 환경에 맞게 수정

--- frontend/src/api/axiosInstance.ts
+++ frontend/src/api/axiosInstance.ts
@@ -3,7 +3,7 @@ import useAuthStore from '../hooks/useAuthStore'; // zustand 스토어 import
 
 //export const API_BASE_URL = 'http://localhost:8080';
 //export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || '/';
-export const API_BASE_URL = 'http://upbid.duckdns.org';
+export const API_BASE_URL = '/';
 
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
