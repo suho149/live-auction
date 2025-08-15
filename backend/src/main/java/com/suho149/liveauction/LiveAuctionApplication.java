f3fe70ad82c13626cc6d3aa099cf4217a8acbfe0
suho <yousuho1232@naver.com>
2025-08-15 10:10:20 +0900

    feat: Docker Compose 파일에 서울 시간대 설정 추가 및 애플리케이션 시작 시 기본 시간대 설정

--- backend/src/main/java/com/suho149/liveauction/LiveAuctionApplication.java
+++ backend/src/main/java/com/suho149/liveauction/LiveAuctionApplication.java
@@ -1,14 +1,22 @@
 package com.suho149.liveauction;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.util.TimeZone;
+
 @EnableScheduling
 @SpringBootApplication
 @EnableCaching
 public class LiveAuctionApplication {
+
+	@PostConstruct
+	public void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiveAuctionApplication.class, args);
